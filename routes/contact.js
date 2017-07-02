ef13e1632dba9965f2c82b0e4f9f8bf8a2a77e69
Michael Lee <mlee@vellichorlabs.com>
2017-07-02 02:15:18 -0500

    commented out nodemailer

--- routes/contact.js
+++ routes/contact.js
@@ -1,46 +1,46 @@
 var express = require('express'),
     router = express.Router(),
-    nodemailer = require('nodemailer'),
+    // nodemailer = require('nodemailer'),
     password = process.env.BLOGPASSWORD;
 
 router.get('/', function(req, res, next) {
     res.render('contact', { title: 'Contact', msg: 'Feel free to leave a message  =)' });
 });
 
-router.post('/sendemail', function(req, res, next) {
-    var transporter = nodemailer.createTransport({
-        service: 'Gmail',
-        auth: {
-            user: 'emailmecorollari@gmail.com',
-            pass: password
-        }
-    });
+// router.post('/sendemail', function(req, res, next) {
+//     var transporter = nodemailer.createTransport({
+//         service: 'Gmail',
+//         auth: {
+//             user: 'emailmecorollari@gmail.com',
+//             pass: password
+//         }
+//     });
 
-    var mailOptions = {
-        from: req.body.name + ' &lt;' + req.body.email +
-            '&gt;',
-        to: 'vlmlee2@gmail.com',
-        subject: req.body.subject,
-        text: req.body.bodytext
-    };
+//     var mailOptions = {
+//         from: req.body.name + ' &lt;' + req.body.email +
+//             '&gt;',
+//         to: 'vlmlee2@gmail.com',
+//         subject: req.body.subject,
+//         text: req.body.bodytext
+//     };
 
-    transporter.sendMail(mailOptions, function(err, info) {
-        if (err) {
-            res.render('contact', {
-                title: 'Contact',
-                msg: 'An error occurred, message was not sent. =(',
-                err: true,
-                page: 'contact'
-            });
-        } else {
-            res.render('contact', {
-                title: 'Contact',
-                msg: 'Message was sent! =)',
-                err: false,
-                page: 'contact'
-            });
-        }
-    });
-});
+//     transporter.sendMail(mailOptions, function(err, info) {
+//         if (err) {
+//             res.render('contact', {
+//                 title: 'Contact',
+//                 msg: 'An error occurred, message was not sent. =(',
+//                 err: true,
+//                 page: 'contact'
+//             });
+//         } else {
+//             res.render('contact', {
+//                 title: 'Contact',
+//                 msg: 'Message was sent! =)',
+//                 err: false,
+//                 page: 'contact'
+//             });
+//         }
+//     });
+// });
 
 module.exports = router;
